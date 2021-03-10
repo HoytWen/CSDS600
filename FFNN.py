263f70484dc4b8cd7d1b5c5f3344a35aeaffdb29
Qianlong Wen <38039677+HoytWen@users.noreply.github.com>
2021-03-09 20:29:05 -0500

    Add files via upload

---
+++ FFNN.py
@@ -0,0 +1,88 @@
+# FFNN.py
+#
+# Licensing Information:  You are free to use or extend this project for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to The Ohio State University, including a link to http://aritter.github.io/courses/5525_fall19.html
+#
+# Attribution Information: This assignment was developed at The Ohio State University
+# by Alan Ritter (ritter.1492@osu.edu).
+
+import sys
+
+import numpy as np
+from Eval import Eval
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+
+from imdb import IMDBdata
+
+class FFNN(nn.Module):
+    def __init__(self, X, Y, VOCAB_SIZE, DIM_EMB=10, NUM_CLASSES=2):
+        super(FFNN, self).__init__()
+        (self.VOCAB_SIZE, self.DIM_EMB, self.NUM_CLASSES) = (VOCAB_SIZE, DIM_EMB, NUM_CLASSES)
+        #TODO: Initialize parameters.
+        self.E = nn.Embedding(VOCAB_SIZE, DIM_EMB)
+        self.R = nn.ReLU()
+        self.W = nn.Linear(DIM_EMB, NUM_CLASSES)
+        self.softmax = nn.Softmax(dim=0)
+        nn.init.xavier_uniform_(self.E.weight)
+        nn.init.xavier_uniform_(self.W.weight)
+
+
+    def forward(self, X, train=True):
+        #TODO: Implement forward computation.
+        embedding = self.E(X)
+        hidden_layer = torch.sum(self.R(embedding), dim = 0) 
+        output = self.W(hidden_layer)
+
+        return self.softmax(output)
+
+
+def Eval_FFNN(X, Y, mlp):
+    num_correct = 0
+    for i in range(len(X)):
+        logProbs = mlp.forward(X[i], train=False)
+        pred = torch.argmax(logProbs)
+        if pred == Y[i]:
+            num_correct += 1
+    print("Accuracy: %s" % (float(num_correct) / float(len(X))))
+
+def Train_FFNN(X, Y, vocab_size, n_iter):
+    print("Start Training!")
+    mlp = FFNN(X, Y, vocab_size)
+    #TODO: initialize optimizer.
+    optimizer = optim.Adam(mlp.parameters(), lr=0.001)
+    # batch_size = 32
+    
+    for epoch in range(n_iter):
+        # shuffled_i = list(range(0,len(Y)))
+        # random.shuffle(shuffled_i)
+        total_loss = 0.0
+        for i in range(len(Y)):
+            x = X[i]
+            y_onehot = torch.zeros(mlp.NUM_CLASSES)
+            # y_onehot[int(Y[start:start + batch_size])] = 1.
+            y_onehot[int(Y[i])] = 1.
+
+            mlp.zero_grad()
+            probs = mlp.forward(x)
+            loss = torch.sum((probs - y_onehot)**2)
+            total_loss += loss
+            
+            loss.backward()
+            optimizer.step()
+            #TODO: compute gradients, do parameter update, compute loss.
+        print(f"loss on epoch {epoch} = {total_loss}")
+    return mlp
+
+if __name__ == "__main__":
+    train = IMDBdata("%s/train" % sys.argv[1])
+    train.vocab.Lock()
+    test  = IMDBdata("%s/dev" % sys.argv[1], vocab=train.vocab)
+    
+    mlp = Train_FFNN(train.XwordList, (train.Y + 1.0) / 2.0, train.vocab.GetVocabSize(), int(sys.argv[2]))
+    Eval_FFNN(test.XwordList, (test.Y + 1.0) / 2.0, mlp)
---
+++ dtree.py
@@ -0,0 +1,170 @@
+"""
+Group 22: Qianlong Wen (qxw294), Jianan Zhao (jxz1244)
+First Commit: Sep 18, 2020
+"""
+import argparse
+from utils import *
+from data_loader import *
+
+
+class DecisionTree():
+    # Decision tree (ID3 or C4.5), along with some train and evaluate functions.
+    def __init__(self, args, f_info):
+        self.dataset = args.data_path.split('/')[-1]
+        self.__dict__.update(args.__dict__)
+        # Init max depth of the tree
+        n_feat = len(f_info) - 2  # indexs and labels are not features
+        if args.t_depth <= 0:  # corner cases: 0
+            self.max_depth = n_feat  # full tree
+        else:
+            self.max_depth = args.t_depth
+        self.method = 'information_gain' if args.gain_method == 0 else 'gain_ratio'
+        self.out_file_name = get_dtree_file_name(self)
+        self.f_info = f_info  # schema
+        self.tree = None
+        self.cand_feat_list = [list(range(1, len(f_info) - 1)) for _ in range(self.max_depth)]
+
+    def print_tree(self):
+        # Print decision tree
+        return f'{self.max_depth}-depth-DecisionTree:\n {dict_printer(self.tree)}'
+
+    def train(self, data, fold=0):
+        model_file_name = get_model_file_name(self, fold)
+        if self.load_model == 1 and os.path.isfile(model_file_name):
+            self.tree = load_pickle(model_file_name)
+        else:
+            # Train the decision tree using data given
+            self.tree = self._gen_decision_tree(data, 0)
+            save_pickle(self.tree, model_file_name)
+
+    def eval(self, data):
+        # Evaluate the decision tree using data given
+        prediction = [self.predict(data[i, :]) for i in range(data.shape[0])]
+        label = data[:, -1]
+        return sum(prediction == label) / len(label)
+
+    def predict(self, data):
+        return self._predict(self.tree, data)
+
+    def _predict(self, tree, example):
+        # Predict the label of exmaple given
+        if isinstance(tree, dict):  # root or internal nodes
+            # find which tree to move on by the conditions
+            for condition in tree['children']:
+                if eval(f"example[tree['feat_id']] {condition}"):
+                    return self._predict(tree['children'][condition], example)
+            return ValueError(f'Conditions not met for {example}')
+        else:  # leaf node, prediction
+            return tree
+
+    def _remove_feat(self, feat_to_split, depth):
+        return self.cand_feat_list[depth].remove(feat_to_split)
+
+    def _gen_decision_tree(self, data, cur_depth):
+        """
+        Generate the tree iteratively
+        :param data: data to fit
+        :param cur_depth: current depth of the tree
+        :return: tree of left and right child or the prediction
+        """
+        label = data[:, -1]
+        print(type(23432))
+        print(len(self.f_info))
+        # ! Case 1: Leaf nodes -> generate predictions
+        # Case 1.1: pure node -> return label as prediction
+        if len(np.unique(label)) == 1:
+            return int(label[0])
+
+        # Case 1.2: Max depth reached -> vote
+        if cur_depth == self.max_depth:
+            return int(sum(label == 1) > sum(label == 0))
+
+        # Case 1.3: Pre-pruning
+        if self.pre_pruning >= 1 and data.shape[0] < self.pre_pruning:
+            return int(sum(label == 1) > sum(label == 0))
+
+        # ! Case 2: Root node or internal nodes -> generate child decision trees
+        # Step 1: calculate entropy and find feature to split
+        if self.method == 'information_gain':
+            info_gain_dict = {feat_id: info_gain(data, feat_id, self.f_info[feat_id].type)[0]
+                              for feat_id in self.cand_feat_list[cur_depth]}
+        elif self.method == 'gain_ratio':
+            info_gain_dict = {feat_id: info_gain_ratio(data, feat_id, self.f_info[feat_id].type)
+                              for feat_id in self.cand_feat_list[cur_depth]}
+        feat_to_split = get_max_ind_in_dict(info_gain_dict)
+        feat_type_to_split = self.f_info[feat_to_split].type
+        # Step 2: split data and remove feature (nominal attributes only)
+        if feat_type_to_split == 'NOMINAL':
+            data_splits = split_data_nominal(data, feat_to_split)
+            self._remove_feat(feat_to_split, cur_depth)
+        elif feat_type_to_split == 'CONTINUOUS':
+            _, threshold = find_threshold_continuous(data, feat_to_split)
+            data_splits = split_data_by_threshold(data, feat_to_split, threshold)
+        # Corner cases: splits not valid (can not split), return voting results
+        for split in data_splits:
+            if data_splits[split].shape[0] == 0:
+                return int(sum(label == 1) > sum(label == 0))
+        # Step 3: generate tree
+        # if np.unique(data_splits.values()) == 1:
+        #     return float(int(label[0]))
+        return {'feat_id': feat_to_split, 'children':
+            {condition: self._gen_decision_tree(data_splits[condition], cur_depth + 1)
+             for condition in data_splits}}
+
+
+def main(args, n_fold=5):
+    """
+    Train a decision tree and evaluate it afterwards.
+    :param args: input arguments specified by user
+    :param n_fold: number of folds for cross validation, default 5
+    :return: None, results are printed to std_out
+    """
+    data, f_info = load_data(args.data_path)
+
+    if args.cv_flag == 0:  # n-fold cross validation
+        data_splits = split_data_cross_validation(data, n_fold)
+        acc_list = []
+        for fold in range(n_fold):
+            if args.log_level >= 1: print(f'Training Fold {fold}...')
+            train_data, test_data = data_splits[fold]
+            model = DecisionTree(args, f_info)
+            model.train(train_data, fold)
+            acc_list.append(model.eval(test_data))
+            print_and_save_output(acc_list[fold], model, f_info, fold)
+        if args.log_level >= 1: print(f'Mean Accuracy = {(sum(acc_list) / n_fold):4f}')
+    else:
+        model = DecisionTree(args, f_info)
+        model.train(data)
+        acc = model.eval(data)
+        print_and_save_output(acc, model, f_info)
+    if args.log_level >= 1: print(f'{model.print_tree()}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+<<<<<<< HEAD
+    data_root = '/Users/wenqianlong/2020_fall_440_22/P1/data'
+=======
+    data_root = '/home/zja/PyProject/CSDS440/data'
+>>>>>>> 4867eb540a7aa8bf25870ce91c1b6b2ab5d7c32a
+    dataset = 'voting'
+    dataset = 'volcanoes'
+    parser.add_argument('--data_path', type=str, default=f'{data_root}/{dataset}/{dataset}',
+                        help='The directory of data')
+    parser.add_argument('--cv_flag', type=int, default=0, help='Use cross validation or not (0 or 1)')
+<<<<<<< HEAD
+    parser.add_argument('--t_depth', type=int, default=16, help='Depth of the decision tree')
+=======
+    parser.add_argument('--t_depth', type=int, default=1, help='Depth of the decision tree')
+>>>>>>> d98151910fbb83a8b0aa2d40634ab8949fd0f18b
+    parser.add_argument('--gain_method', type=int, default=1,
+                        help='If 0, use information gain as the split criterion. If 1, use gain ratio.')
+    parser.add_argument('--pre_pruning', type=int, default=0,
+                        help='Pre-pruning or not (size or 0)')
+    # ! Other options used for debug
+    parser.add_argument('--load_model', type=int, default=0, help='Load model')
+    parser.add_argument('--log_level', type=int, default=0, help='-1 (no log), 0(default log), or 1 (more)')
+    parser.add_argument('--save_results', type=int, default=0, help='Save result or not (1 or 0)')
+    parser.add_argument('--out_file_postfix', type=str, default='')
+    args = parser.parse_args()
+    main(args)
